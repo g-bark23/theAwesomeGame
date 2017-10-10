dddfdc26f2b885443a6fe880d0261c5f848d38df
pharrett23 <pharrett23@gmail.com>
2017-10-09 20:41:08 -0600

    game board working

---
+++ gamePage.php
@@ -0,0 +1,75 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+		<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		var drawGrid = function(w, h, id) {
+		    var canvas = document.getElementById(id);
+		    var ctx = canvas.getContext('2d');
+		    console.log(ctx);
+		    ctx.canvas.width = w;
+		    ctx.canvas.height = h;
+
+		    for (x = 0; x <= w; x += 10) {
+		        ctx.moveTo(x, 0);
+		        ctx.lineTo(x, h);
+		        for (y = 0; y <= h; y += 10) {
+		            ctx.moveTo(0, y);
+		            ctx.lineTo(w, y);
+		        }
+		    }
+		    ctx.stroke();
+		};
+
+		function storeGuess(event){
+			var x = event.offsetX;
+			var y = event.offsetY;
+			guessX = x;
+			guessY = y; 
+
+			console.log("x coords: " + guessX + ", " + "y coords: " + guessY);
+			findCell(guessX, guessY);
+		}
+
+		function findCell(x, y){
+			var xValue = Math.floor(x/10);
+			var yValue = Math.floor(y/10);
+
+			console.log("Final X: " + xValue + ", Final Y: " + yValue);
+			colorCell(xValue, yValue);
+		}
+
+		function colorCell(x, y){
+			var canvas = document.getElementById("myCanvas");
+			var ctx = canvas.getContext('2d');
+
+			ctx.beginPath();
+			ctx.rect((x * 10) + 1, (y * 10) + 1, 8, 8);
+			ctx.fillStyle = "red";
+			ctx.fill();
+		}
+
+		function clearBoard(){
+			drawGrid(300, 300, "myCanvas")
+		}
+
+	</script> 
+</head>
+<body onload="drawGrid(300, 300, 'myCanvas')">
+	<div id="myDivision" class="container">
+		<canvas id="myCanvas" onclick="storeGuess(event)"></canvas>
+	</div>	
+	<div class="container"><button type="button" onclick="clearBoard()" class="btn btn-danger">Clear</button></div>
+</body>
+</html>
