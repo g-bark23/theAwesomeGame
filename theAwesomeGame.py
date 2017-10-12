6d592b55f35bf447a13e94a8a2830b8e53b22066
Taylor Earl <taylor@chriss-mbp.users.weber.edu>
2017-10-11 19:49:32 -0600

    python

--- theAwesomeGame.py
+++ theAwesomeGame.py
@@ -4,10 +4,10 @@ import mysql.connector
 
 
 config = {
-	'user':'taylor',
+	'user':'W01186504',
 	'password':'Taylorcs!',
 	'host':'127.0.0.1',
-	'database':'theAwesomeGame'
+	'database':'W01186504'
 }
 
 #Board is 30x30 (33x33)
@@ -17,126 +17,126 @@ config = {
 ####	y (int)
 ####	value (int) 1 alive/0 dead
 
-
-try:
-	cnx = mysql.connector.connect(**config)
-					
-except mysql.connector.Error as err:
-	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-		print("Something is wrong with your user name or password")
-	elif err.errno == errorcode.ER_BAD_DB_ERROR:
-		print("Database does not exist")
+while true:
+	try:
+		cnx = mysql.connector.connect(**config)
+						
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with your user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
 	else:
-		print(err)
-else:
-	cnx.close()
-	
-cursor = cnx.cursor()
+		cnx.close()
+		
+	cursor = cnx.cursor()
 
-query = ("SELECT x, y FROM dataPoints")
+	query = ("SELECT x, y FROM dataPoints")
 
-w, h = 33, 33;
-oldGameBoard = [[0 for x in range(w)] for y in range(h)] 
-newGameBoard = [[0 for x in range(w)] for y in range(h)] 
+	w, h = 33, 33;
+	oldGameBoard = [[0 for x in range(w)] for y in range(h)] 
+	newGameBoard = [[0 for x in range(w)] for y in range(h)] 
 
-cursor.execute(query)
+	cursor.execute(query)
 
-for (x, y) in cursor:
-	oldGameBoard[x][y] = 1
-	
-for(x, y) in cursor:
-	count = 0;
-	#check row above
-	if(oldGameBoard[x-1][y-1] == 1):
-		count++;
-	
-	if(oldGameBoard[x][y-1] == 1):
-		count++;
-	
-	if(oldGameBoard[x+1][y-1] == 1):
-		count++;
-	
-	
-	#check row
-	if(oldGameBoard[x-1][y] == 1):
-		count++;
-	
-	#if(oldGameBoard[x][y] == 1){
-	#	count++;
-	#}
-	if(oldGameBoard[x+1][y] == 1):
-		count++;
-	
-	
-	#check row below
-	if(oldGameBoard[x-1][y+1] == 1):
-		count++;
-	
-	if(oldGameBoard[x][y+1] == 1):
-		count++;
-	
-	if(oldGameBoard[x+1][y+1] == 1):
-		count++;
-	
-	#check all live cells
-	if(count < 2):
-		newGameBoard[x][y] = 0
-	elif(count < 4 && count >= 2):
-		newGameBoard[x][y] = 1	
-	else:
-		newGameBoard[x][y] = 0
+	for (x, y) in cursor:
+		oldGameBoard[x][y] = 1
+		
+	for(x, y) in cursor:
+		count = 0;
+		#check row above
+		if(oldGameBoard[x-1][y-1] == 1):
+			count++
+		
+		if(oldGameBoard[x][y-1] == 1):
+			count++
+		
+		if(oldGameBoard[x+1][y-1] == 1):
+			count++
+		
+		
+		#check row
+		if(oldGameBoard[x-1][y] == 1):
+			count++
+		
+		#if(oldGameBoard[x][y] == 1){
+		#	count++;
+		#}
+		if(oldGameBoard[x+1][y] == 1):
+			count++
+		
+		
+		#check row below
+		if(oldGameBoard[x-1][y+1] == 1):
+			count++
+		
+		if(oldGameBoard[x][y+1] == 1):
+			count++
+		
+		if(oldGameBoard[x+1][y+1] == 1):
+			count++
+		
+		#check all live cells
+		if(count < 2):
+			newGameBoard[x][y] = 0
+		elif(count < 4 && count >= 2):
+			newGameBoard[x][y] = 1	
+		else:
+			newGameBoard[x][y] = 0
 
-for(x in range(1, 33)):
-	for(y in range(1, 33)):
-			count = 0;
-			#check row above
-			if(oldGameBoard[x-1][y-1] == 1):
-				count++;
-			
-			if(oldGameBoard[x][y-1] == 1):
-				count++;
-			
-			if(oldGameBoard[x+1][y-1] == 1):
-				count++;
-			
-			
-			#check row
-			if(oldGameBoard[x-1][y] == 1):
-				count++;
-			
-			#if(oldGameBoard[x][y] == 1){
-			#	count++;
-			#}
-			if(oldGameBoard[x+1][y] == 1):
-				count++;
-			
-			
-			#check row below
-			if(oldGameBoard[x-1][y+1] == 1):
-				count++;
-			
-			if(oldGameBoard[x][y+1] == 1):
-				count++;
-			
-			if(oldGameBoard[x+1][y+1] == 1):
-				count++;
-			
-			if(count == 3):
-				newGameBoard[x][y] = 1
+	for(x in range(1, 33)):
+		for(y in range(1, 33)):
+				count = 0;
+				#check row above
+				if(oldGameBoard[x-1][y-1] == 1):
+					count++;
+				
+				if(oldGameBoard[x][y-1] == 1):
+					count++;
+				
+				if(oldGameBoard[x+1][y-1] == 1):
+					count++;
+				
+				
+				#check row
+				if(oldGameBoard[x-1][y] == 1):
+					count++;
+				
+				#if(oldGameBoard[x][y] == 1){
+				#	count++;
+				#}
+				if(oldGameBoard[x+1][y] == 1):
+					count++;
+				
+				
+				#check row below
+				if(oldGameBoard[x-1][y+1] == 1):
+					count++;
+				
+				if(oldGameBoard[x][y+1] == 1):
+					count++;
+				
+				if(oldGameBoard[x+1][y+1] == 1):
+					count++;
+				
+				if(count == 3):
+					newGameBoard[x][y] = 1
 
-insertSql = "";				
-#for(x in range(1, 33)):
-#	for(y in range(1, 33)):
-#		if(newGameBoard[x][y] == 1):
-#			insertSql += "UPDATE dataPoints SET value=1 WHERE x=" + x + " AND y=" + y + ";"
-#		else:
-#			insertSql += "UPDATE dataPoints SET value=0 WHERE x=" + x + " AND y=" + y + ";"
-			
-			
-for(x in range(1, 33)):
-	for(y in range(1, 33)):
-		if(newGameBoard[x][y] != oldGameBoard[x][y]):
-			insertSql += "UPDATE dataPoints SET value=" + newGameBoard[x][y] + " WHERE x=" + x + " AND y=" + y + ";"
-			
-cursor.execute(insertSql)
-cursor.close()
+	insertSql = "";				
+	#for(x in range(1, 33)):
+	#	for(y in range(1, 33)):
+	#		if(newGameBoard[x][y] == 1):
+	#			insertSql += "UPDATE dataPoints SET value=1 WHERE x=" + x + " AND y=" + y + ";"
+	#		else:
+	#			insertSql += "UPDATE dataPoints SET value=0 WHERE x=" + x + " AND y=" + y + ";"
+				
+				
+	for(x in range(1, 33)):
+		for(y in range(1, 33)):
+			if(newGameBoard[x][y] != oldGameBoard[x][y]):
+				insertSql += "UPDATE dataPoints SET value=" + newGameBoard[x][y] + " WHERE x=" + x + " AND y=" + y + ";"
+				
+	cursor.execute(insertSql)
+	cursor.close()
