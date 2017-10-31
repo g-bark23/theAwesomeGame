e7c524f96d0a6a83c4d57027886190476acd91d5
Taylor Earl <taylor@vpn-137-190-250-69.weber.edu>
2017-10-30 22:36:48 -0600

    python

--- gamePage.php
+++ gamePage.php
@@ -104,8 +104,8 @@
 
 			console.log("Final X: " + xValue + ", Final Y: " + yValue);
 
-		    xCoord = xValue;
-		    yCoord = yValue;
+		    xCoord = xValue + 1;
+		    yCoord = yValue + 1;
 		}
 
 		function colorCell(x, y, fillColor){
